acf3630a1d5b126546df3369fd9107c1c7f0d85d
David <8584751@qq.com>
2017-12-28 01:08:53 +0800

    增加说明文字

--- MyProject/GitHubTest/GitHubTest/Form1.Designer.cs
+++ MyProject/GitHubTest/GitHubTest/Form1.Designer.cs
@@ -28,33 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(102, 75);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Hello World";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 162);
+            this.button1.Location = new System.Drawing.Point(83, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(83, 202);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(343, 271);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "Clone就是将远程库的代码拷贝到本地。\n\ncommit将工作空间修改提交到本地库。\n\npush将本地库修改提交到远程库。\n\npull 从远程拉取最新版本 到本地" +
+    "  自动合并 merge\n\nfetch   从远程获取最新版本 到本地   不会自动合并 merge\n\nreset 将当前版本回退到选定的版本\n\ncheckou" +
+    "t 创建分支，切换分支\n\ntag 是给版本打个标记，方便用于识别查看等操作。";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hello World";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 384);
+            this.ClientSize = new System.Drawing.Size(545, 627);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -65,9 +78,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
