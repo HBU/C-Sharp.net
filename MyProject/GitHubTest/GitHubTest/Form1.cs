9718c78bd246c728da1a8c8d5b21fdb4df844b9c
David <8584751@qq.com>
2018-01-03 09:47:39 +0800

    网络一班测试VS-GitHub

--- MyProject/GitHubTest/GitHubTest/Form1.cs
+++ MyProject/GitHubTest/GitHubTest/Form1.cs
@@ -26,5 +26,10 @@ namespace GitHubTest
         {
             MessageBox.Show("Hello world ");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world ");
+        }
     }
 }
