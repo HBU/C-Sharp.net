e8ec07e59a09757b781e9a4b01e7cc1fea54be24
David <cmc_weiyg@163.com>
2019-07-11 04:06:24 +0800

    core

---
+++ SourceCode/DavidCode/C1_6_Console_Core/C1_6_Console_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C1_6_Console_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
