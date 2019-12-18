e8c116190eb3981d0d7e059c75c6ce9aa5d585d3
David <cmc_weiyg@163.com>
2019-12-18 22:46:21 +0800

    update

--- SourceCode/第2版（2-9章）代码/Test9/Test9/Test9_1.cs
+++ SourceCode/第2版（2-9章）代码/Test9/Test9/Test9_1.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 using System.Threading;//注意对命名空间的引用
+
 namespace Test9_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Thread.CurrentThread.Name = "主线程";//修改当前线程的线程名
-            Thread objThread = new Thread(new ThreadStart(ActionMethod));//创建线程
-            objThread.Name = "子线程";//设置线程名
-            objThread.Start();//启运线程
-            ActionMethod();//在主线程中调用ActionMethod()
+            Thread.CurrentThread.Name = "主线程----------";                                       //修改 当前线程 的线程名
+
+            Thread objThread0 = new Thread(new ThreadStart(ActionMethod));           //创建线程
+            objThread0.Name = "----------子线程0";                                                           //设置线程名
+            objThread0.Start();                                                                                            //启动线程
+
+            Thread objThread1 = new Thread(new ThreadStart(ActionMethod));           //创建线程
+            objThread1.Name = "----------子线程1";                                                           //设置线程名
+            objThread1.Start();                                                                                            //启动线程
+
+            //Thread.Sleep(10);
+            ActionMethod();                                                                                              //在主线程中调用ActionMethod()
         }
+
         static void ActionMethod()//线程调用的方法
         {
-            for (int count = 1; count <= 5; count++)
+            for (int count = 1; count <= 50; count++)
             {
                 Console.WriteLine("{0}:第{1}次", Thread.CurrentThread.Name,count);
-                
-
+                Thread.Sleep(100);
             }
         }
     }
--- SourceCode/第2版（2-9章）代码/Test9/Test9/Test9_7.cs
+++ SourceCode/第2版（2-9章）代码/Test9/Test9/Test9_7.cs
@@ -1,36 +1,45 @@
 ﻿using System;
 using System.Threading;
+
 namespace Test9_7
 {
     public delegate string AsyncDelegate(int callTime); //定义一个委托，用来提供异步调用的接口
+
     class Test9_7
     {
         static void Main(string[] args)
         {
-            AsyncDemo ad = new AsyncDemo();
-            AsyncDelegate dlgt = new AsyncDelegate(ad.TestMethod); //封装要异步调用的方法
-           //开始异步调用（指定该方法的执行时间—3秒），并指定回调方法
-            IAsyncResult ar = dlgt.BeginInvoke(3000, new AsyncCallback(CallbackMethod), dlgt); 
-            Console.WriteLine("主线程继续工作...");  //程序继续执行
-            Thread.Sleep(1000);   //模拟主程序需要执行的时间—1秒
-            Console.WriteLine("主线程工作完成，执行了1秒，等待异步调用完成....");
+            AsyncDemo asyncDemo = new AsyncDemo();
+            AsyncDelegate asyncDelegate = new AsyncDelegate(asyncDemo.TestMethod); //封装要异步调用的方法
+
+            Console.WriteLine(DateTime.Now.ToString() + "  ### 主线程开始工作");
+            Thread.Sleep(3000);//三秒输出一行，便于观察
+
+            //开始异步调用（指定该方法的执行时间—10秒），并指定回调方法
+            IAsyncResult iAsyncResult = asyncDelegate.BeginInvoke(10000, new AsyncCallback(CallbackMethod), asyncDelegate);
+            Thread.Sleep(3000);//三秒输出一行，便于观察
+
+            Console.WriteLine(DateTime.Now.ToString() + "  ### 主线程继续工作");  //程序继续执行
+            Thread.Sleep(5000);   //模拟主程序需要执行的时间—5秒----注意：与异步执行的程序同时执行，节约了时间
+            Console.WriteLine(DateTime.Now.ToString() + "  ### 主线程工作完成：执行了3秒，等待异步调用完成----");
             Console.ReadKey();
         }
-        static void CallbackMethod(IAsyncResult ar)  //异步调用结束后的回调方法
-        {
-            //从异步操作的状态中提取AsyncDelegate委托
-            AsyncDelegate dlgt = (AsyncDelegate)ar.AsyncState; 
-            string result = dlgt.EndInvoke(ar);//获取异步调用的结果
-            Console.WriteLine("异步调用完成,{0}!",result);
+
+        static void CallbackMethod(IAsyncResult iAsyncResult)  //异步调用结束后的回调方法
+        {           
+            AsyncDelegate asyncDelegate = (AsyncDelegate)iAsyncResult.AsyncState;  //从异步操作的状态中提取AsyncDelegate委托
+            string result = asyncDelegate.EndInvoke(iAsyncResult);//获取异步调用的结果
+            Console.WriteLine(DateTime.Now.ToString() + "  @@@ 异步调用完成：{0}!", result);
         }
     }
+
     class AsyncDemo
     {
         public string TestMethod(int callTime)   //异步调用的方法
         {
-            Console.WriteLine("异步调用的方法开始...");
+            Console.WriteLine(DateTime.Now.ToString() + "  @@@ 异步调用的方法开始...");
             Thread.Sleep(callTime);    //模拟该方法需要执行的时间—3秒
-            return "方法完成需要的时间是" + callTime / 1000 + "秒";
+            return " @@@ 方法完成需要的时间是" + callTime / 1000 + "秒";
         }
     }
 }
